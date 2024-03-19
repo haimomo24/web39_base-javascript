8b3a7c5d662d72d0b411b24cfcee92a25b6ae2cc
haimomo24 <135099315+haimomo24@users.noreply.github.com>
2024-03-19 21:11:29 +0700

    update

---
+++ buoi1/assets/js/main.js
@@ -0,0 +1,61 @@
+// console.log("hello") 
+
+// bien toan cuc 
+
+
+// let name = "Nguyen van a ";
+// let age = 10;
+// console.log(name);
+// console.log(age);
+
+
+/* 
+pham vi bien trong 1 bien function 
+
+
+*/
+// khai bao vd1 
+// function hello() {
+//     // logic 
+//     console.log("helo 500 ae ");
+// }
+
+
+// // thuc thi  
+// hello();
+
+// // khai bao vd2 
+// function hi() {
+//     let count = 39;
+//     // console.log("hi 123")
+//     console.log(count);
+// }
+// hi();
+
+
+
+// vd3 : khai bao 1 function sayHello()
+// function sayHello() {
+
+//      city = "ha noi ";
+//     console.log("buoi toi tot lanh ");
+// }
+
+// sayHello();
+// console.log (city);
+
+// cac kieu du lieu co ban 
+// kieu string 
+
+// let a = "hom nay troi mua " ;
+// let b = 'thoi tiet that thuong ';
+// console.log(a);
+// console.log(b);
+
+// chus is 
+
+// let message = "xin chao \"cac ban \"hihihi ";
+// let email = 'anh ten la "hai" '
+// console.log(message);
+// console.log(email);
+
